c81a68f9fbd1225241414a8366b2ee8e10dcc79a
Lusain_Kim <sain9212@gmail.com>
2018-03-18 17:41:17 +0900

    update basic color picker

---
+++ WpfColorPicker/Color.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LusColor
+{
+    public struct RGB
+    {
+        public byte b;
+        public byte g;
+        public byte r;
+
+        public RGB(byte R, byte G, byte B)
+        {
+            r = R;
+            g = G;
+            b = B;
+        }
+    }
+
+    public struct HSV
+    {
+        public float h;
+        public float s;
+        public float v;
+
+        public HSV(float H, float S, float V)
+        {
+            h = H;
+            s = S;
+            v = V;
+        }
+    }
+
+    public class Color
+    {
+
+        private HSV _hsv = new HSV();
+        private RGB _rgb = new RGB();
+
+        public HSV hsv
+        {
+            get { return _hsv; }
+            set { _hsv = value; _rgb = Convert(value); }
+        }
+
+        public RGB rgb
+        {
+            get { return _rgb; }
+            set { _rgb = value; _hsv = Convert(value); }
+        }
+
+        public Color()
+        {
+            rgb = new RGB(0, 0, 0);
+            hsv = Convert(rgb);
+        }
+
+        static public RGB Convert(HSV hsv)
+        {
+            RGB rgb = new RGB();
+
+            byte v = (byte)(255 * hsv.v);
+            // achromatic (grey)
+            if (hsv.s == 0.0f)
+            {
+                rgb.r = v;
+                rgb.g = v;
+                rgb.b = v;
+                return rgb;
+            }
+
+            // sector 0 to 5
+            hsv.h /= 60.0f;
+
+            int i = (int)Math.Floor(hsv.h);
+
+            // factorial part of h
+            float f = hsv.h - i;
+            byte p = (byte)(255 * (hsv.v * (1.0f - hsv.s)));
+            byte q = (byte)(255 * (hsv.v * (1.0f - hsv.s * f)));
+            byte t = (byte)(255 * (hsv.v * (1.0f - hsv.s * (1.0f - f))));
+
+            switch (i)
+            {
+                case 0: rgb.r = v; rgb.g = t; rgb.b = p; break;
+                case 1: rgb.r = q; rgb.g = v; rgb.b = p; break;
+                case 2: rgb.r = p; rgb.g = v; rgb.b = t; break;
+                case 3: rgb.r = p; rgb.g = q; rgb.b = v; break;
+                case 4: rgb.r = t; rgb.g = p; rgb.b = v; break;
+                case 5: rgb.r = v; rgb.g = p; rgb.b = q; break;
+            }
+
+            return rgb;
+        }
+
+        static public HSV Convert(RGB rgb)
+        {
+            HSV hsv = new HSV();
+
+            float min = Math.Min(Math.Min(rgb.r, rgb.g), rgb.b) / 255.0f;
+            float max = Math.Max(Math.Max(rgb.r, rgb.g), rgb.b) / 255.0f;
+            float delta = max - min;
+
+            // between yellow & magenta
+            if (rgb.r / 255.0f == max) hsv.h = 0.0f + (float)(rgb.g - rgb.b) / delta;
+            // between cyan & yellow
+            else if (rgb.g / 255.0f == max) hsv.h = 2.0f + (float)(rgb.b - rgb.r) / delta;
+            // between magenta & cyan
+            else hsv.h = 4.0f + (float)(rgb.r - rgb.g) / delta;
+
+            // degrees
+            hsv.h *= 60.0f;
+
+            if (hsv.h < 0) hsv.h += 360;
+
+            // s
+            if (max != 0) hsv.s = delta / max;
+            else
+            {
+                // r = g = b = 0		// s = 0, v is undefined
+                hsv.s = 0;
+                hsv.h = -1.0f;
+                return hsv;
+            }
+
+            // v
+            hsv.v = max;
+
+            return hsv;
+        }
+    }
+}
--- WpfColorPicker/MainWindow.xaml.cs
+++ WpfColorPicker/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
+
+using LusColor;
+using System.Reflection;
 
 namespace WpfColorPicker
 {
@@ -20,9 +24,169 @@ namespace WpfColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WriteableBitmap bmp_hsv_board;
+        private WriteableBitmap bmp_hsv_bar;
+
+        private float Hvalue = 0.0f;
+        private LusColor.Color SelectedColor = new LusColor.Color();
+
+
+        private bool IsBarSelect { get; set; }
+
+        public int dpiX;
+        public int dpiY;
+
         public MainWindow()
         {
+            SelectedColor.hsv = new HSV(0, 0, 0);
+            
+            var dpiXProperty = typeof(SystemParameters).GetProperty("DpiX", BindingFlags.NonPublic | BindingFlags.Static);
+            var dpiYProperty = typeof(SystemParameters).GetProperty("Dpi", BindingFlags.NonPublic | BindingFlags.Static);
+
+            dpiX = (int)dpiXProperty.GetValue(null, null);
+            dpiY = (int)dpiYProperty.GetValue(null, null);
+
             InitializeComponent();
         }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+        //  if (e.LeftButton == MouseButtonState.Pressed)
+        //  {
+        //      DragMove();
+        //      return;
+        //  }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+                Application.Current.Shutdown();
+        }
+
+        private void CreateHSVBoard()
+        {
+            HSV hsv = new HSV(Hvalue, 0.0f, 1.0f);
+
+            float stepW = 1.0f / ((float)bmp_hsv_board.Width - 1.0f);
+            float stepH = 1.0f / ((float)bmp_hsv_board.Height - 1.0f);
+
+            try
+            {
+                bmp_hsv_board.Lock();
+                unsafe
+                {
+                    int* pStart = (int*)bmp_hsv_board.BackBuffer;
+
+                    for (int y = 0; y < bmp_hsv_board.PixelHeight; ++y)
+                    {
+                        hsv.s = 0.0f;
+                 
+                        for (int x = 0; x < bmp_hsv_board.PixelWidth; ++x)
+                        {
+                            RGB rgb = LusColor.Color.Convert(hsv);
+                            int pixel = 0;
+                            pixel |= rgb.r << 16;  // R
+                            pixel |= rgb.g << 8;   // G
+                            pixel |= rgb.b << 0;   // B
+
+                            *(pStart) = pixel;
+
+                            pStart++;
+                            hsv.s += stepW;
+                        }
+                        hsv.v -= stepH; 
+                    }
+                    bmp_hsv_board.AddDirtyRect(new Int32Rect(0, 0, bmp_hsv_board.PixelWidth, bmp_hsv_board.PixelHeight));
+                }
+            }
+            finally
+            {
+                bmp_hsv_board.Unlock();
+            }
+        }
+
+        private void CreateHSVBar()
+        {
+            float factor = 360.0f / (float)bmp_hsv_bar.Height;
+
+            unsafe
+            {
+                bmp_hsv_bar.Lock();
+                int* pStart = (int*)bmp_hsv_bar.BackBuffer;
+
+                for (int y = 0; y < bmp_hsv_bar.PixelHeight; ++y)
+                {
+                    RGB rgb = LusColor.Color.Convert(new HSV((bmp_hsv_bar.PixelHeight - (y + 1)) * factor, 1.0f, 1.0f));
+                    int pixel = 0;
+                    pixel |= rgb.r << 16;  // R
+                    pixel |= rgb.g << 8;   // G
+                    pixel |= rgb.b << 0;   // B
+                    for (int x = 0; x < bmp_hsv_bar.PixelWidth; ++x)
+                    {
+                        *(pStart) = pixel;
+                        pStart++;
+                    }
+                }
+
+                bmp_hsv_bar.AddDirtyRect(new Int32Rect(0, 0, bmp_hsv_bar.PixelWidth, bmp_hsv_bar.PixelHeight));
+                bmp_hsv_bar.Unlock();
+            }
+        }
+
+
+        private void Board_Init(object sender, EventArgs e)
+        {
+            bmp_hsv_board = new WriteableBitmap((int)image_board.Width, (int)image_board.Height, dpiX, dpiY, PixelFormats.Bgr32, null);
+            
+            CreateHSVBoard();
+            image_board.Source = bmp_hsv_board;
+            image_board.UpdateLayout();
+        }
+
+        private void Bar_Init(object sender, EventArgs e)
+        {
+            bmp_hsv_bar = new WriteableBitmap((int)image_bar.Width, (int)image_bar.Height, dpiX, dpiY, PixelFormats.Bgr32, null);
+
+            CreateHSVBar();
+            image_bar.Source = bmp_hsv_bar;
+        }
+
+        private void BarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+
+            IsBarSelect = true;
+            float y = (float)(e.GetPosition(image_bar).Y);
+
+            RefreshHSVBoard(y);
+        }
+
+        private void RefreshHSVBoard(float y)
+        {
+            float old = Hvalue;
+            float Height = (float)bmp_hsv_bar.Height;
+            y = Math.Max(Math.Min(Height - y, Height), 0);
+            Hvalue = y * 360.0f / Height ;
+
+            if (Math.Abs(old - Hvalue) > 1.0f)
+            {
+                CreateHSVBoard();
+                image_board.Source = bmp_hsv_board;
+            }
+        }
+
+        private void BarMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsBarSelect) return;
+
+            float y = (float)(e.GetPosition(image_bar).Y);
+            RefreshHSVBoard(y);
+        }
+
+        private void BarMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsBarSelect) return;
+            IsBarSelect = false;
+        }
+
     }
+
 }
